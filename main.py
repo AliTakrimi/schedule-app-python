1c30468936e9da1c9f6c4b2ecd011067a34379cf
Ali Takrimi <takrimia@yahoo.com>
2022-07-03 17:11:05 +0430

    write simple scheduler with schedule package

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import schedule
+import schedule as sch
 import time
+
+def job():
+    print("I'm working ...")
+
+sch.every(10).seconds.do(job)
+
+while True:
+    sch.run_pending()
+    time.sleep(1)
