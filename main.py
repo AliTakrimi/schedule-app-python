7a394db8a03b4ae6813ea7108e165ce211669f63
Ali Takrimi <takrimia@yahoo.com>
2022-09-18 09:06:33 +0430

    changes after long time

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 import schedule as sch
 import time
 
-def job():
-    print("I'm working ...")
+class Scheduler:
+    def __init__(self):
+        self.jobs = []
+    def every(self, interval=1):
+        job = Job(interval)
+        self.jobs.append(job)
+        return job
+class Job:
+    def __init__(self, interval):
+        self.interval = interval
+        self.job_func = None
+        self.last_run = None
+        self.next_run = None
+        self.unit = None
+        self.period = None
 
-def greeting():
-    print("How are you?")
+    @property
+    def second(self):
+        assert self.interval == 1
+        return self.seconds
 
-sch.every(10).seconds.do(job)
-sch.every(20).seconds.do(greeting)
+    @property
+    def seconds(self):
+        self.unit = 'seconds'
+        return self
+
+    def minute(self):
+        assert self.interval == 1
+        return self.minutes
+
+    def minutes(self):
+        self.unit = 'minutes'
+        return self
+
+job = Job(2)
+
+print(job.minutes())
 
 
-while True:
-    sch.run_pending()
-    time.sleep(1)
+# def job():
+#     print("Hello I am ali takrimi ...")
+#
+# sch.every().second.do(job)
+#
+# while True:
+#     sch.run_pending()
+#     time.sleep(1)
